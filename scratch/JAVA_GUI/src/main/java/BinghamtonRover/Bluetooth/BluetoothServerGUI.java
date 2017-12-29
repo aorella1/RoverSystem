24d73f36fc34f1ed2ec3398d8250301efd1b3a28
edlforest <hzheng20@binghamton.edu>
2017-12-29 15:32:10 -0500

    Creating new branch

--- scratch/JAVA_GUI/src/main/java/BinghamtonRover/Bluetooth/BluetoothServerGUI.java
+++ scratch/JAVA_GUI/src/main/java/BinghamtonRover/Bluetooth/BluetoothServerGUI.java
@@ -7,26 +7,38 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.apache.commons.lang3.Validate;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class BluetoothServerGUI extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage){
 
         URL loFXMLPath = getClass().getClassLoader().getResource("BinghamtonRover/Bluetooth/BluetoothGUI.fxml");
+        Validate.notNull(loFXMLPath, "The FXML Resource path is null.");
+        System.out.println("bilibili");
 
         System.out.println("loading FXML from: " + loFXMLPath.getPath());
         FXMLLoader loLoader= new FXMLLoader();
-        Validate.notNull(loFXMLPath, "The FXML Resource path is null.");
-        Parent root = loLoader.load(loFXMLPath);
+
+
+        Parent root = null;
+        try{
+           root = loLoader.load(loFXMLPath);
+        }
+        catch(IOException e){
+          e.printStackTrace();
+            System.exit(1);
+        }
+        Validate.notNull(root);
         primaryStage.setTitle("Bluetooth");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
     @Override
-    public void stop() throws Exception {
+    public void stop() {
 
         System.exit(0);
     }
