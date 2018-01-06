4117e6f5f4938b504fed77d28d4c8619eb2ece6d
edlforest <hzheng20@binghamton.edu>
2018-01-06 18:04:13 -0500

    Add skeleton for unit tests

---
+++ scratch/JAVA_GUI/Test/BinghamtonRover/DemoRunnerTest.java
@@ -0,0 +1,23 @@
+package BinghamtonRover;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DemoRunnerTest {
+
+    @Test
+    public void main() {
+        fail("This test has not been implemented");
+    }
+
+    @Test
+    public void start() {
+        fail("This test has not been implemented");
+    }
+
+    @Test
+    public void stop() {
+        fail("This test has not been implemented");
+    }
+}
