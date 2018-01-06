750dee7cc1cff814a5cfb696184fb9c3ebaa613a
edlforest <hzheng20@binghamton.edu>
2018-01-06 16:13:30 -0500

    Tring to create a new branch, see how the commit goes

--- scratch/JAVA_GUI/Test/BinghamtonRover/Bluetooth/BluetoothClientGUITest.java
+++ scratch/JAVA_GUI/Test/BinghamtonRover/Bluetooth/BluetoothClientGUITest.java
@@ -1,5 +1,16 @@
+package BinghamtonRover.Bluetooth;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BluetoothClientGUITest {
 
+    @Test
+    public void start() {
+    }
+
+    @Test
+    public void stop() {
+    }
 }
