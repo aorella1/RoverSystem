ba4f17a1cdb75232097eca944156629b7e18bcfe
hzheng20 <haosenzheng@gmail.com>
2018-02-14 16:52:07 -0500

    Trying out gauges

--- development/JAVA_DEV_GUI/src/main/java/BinghamtonRover/DemoRunner.java
+++ development/JAVA_DEV_GUI/src/main/java/BinghamtonRover/DemoRunner.java
@@ -7,13 +7,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.commons.lang3.Validate;
 import org.opencv.core.Core;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class DemoRunner extends Application
 {
-    private static String gsFile;
+    private static URL gsFile;
 
     // Load in OpenCV3 libraries
     static
@@ -22,44 +26,52 @@ public class DemoRunner extends Application
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws MalformedURLException
     {
-        gsFile = (args.length > 0) ? args[0] : "./src/main/resources/BinghamtonRover/BinghamtonRover.Monitors/python_output.log.json";
+
+        String lsFilePath = "BinghamtonRover/Monitors/python_output.log.json";
+        gsFile = (args.length > 0) ? new URL(args[0]) : DemoRunner.class.getClassLoader().getResource(lsFilePath);
         launch(args);
     }
 
     @Override
-    public void start(Stage aoPrimaryStage) throws Exception
+    public void start(Stage aoPrimaryStage) throws NullPointerException, IOException
     {
+
+        //Get the path of the FXML file
+        URL loFXMLPath = getClass().getClassLoader().getResource("BinghamtonRover/GuiMain/guiScene.fxml");
+
+
+        //Initialize a new array of Observer
         ArrayList<InformationObserver> laoObservers = new ArrayList<>();
 
+        //Start the GUI
+        FXMLLoader loLoader = new FXMLLoader(loFXMLPath);
+        Parent loRoot = loLoader.load();
+        aoPrimaryStage.setTitle("Rover Controller");
+        aoPrimaryStage.setScene(new Scene(loRoot));
+        aoPrimaryStage.show();
 
-//        System.out.println(GuiController.class.getResource("."));
+        //Get the Controller and assign it to each kinds of monitor
+        GuiController loController = loLoader.getController();
+        Validate.notNull(loController,"GuiController is null");
 
-        FXMLLoader loLoader = new FXMLLoader(GuiController.class.getResource("./guiScene.fxml"));
-
-        GuiController loController = new GuiController();
-
-        loLoader.setController(loController);
-
-        laoObservers.add(new DistanceMonitor());
-        laoObservers.add(new BatteryMonitor());
-        laoObservers.add(new DirectionMonitor());
-
+        laoObservers.add(new DistanceMonitor(loController));
+        laoObservers.add(new BatteryMonitor(loController));
+        laoObservers.add(new DirectionMonitor(loController));
         laoObservers.add(new PressureMonitor(loController));
         laoObservers.add(new TimeMonitor(loController));
         laoObservers.add(new CameraStatusMonitor(loController));
         laoObservers.add(new LocationMonitor(loController));
         laoObservers.add(new TemperatureMonitor(loController));
 
-        FileUpdatingObservable lfuo = new FileUpdatingObservable(gsFile, laoObservers);
-        lfuo.startFileMonitoringThread();
+        FileUpdatingObservable loObservable = new FileUpdatingObservable(gsFile.getPath(), laoObservers);
+        loObservable.startFileMonitoringThread();
 
-        Parent loRoot = (Parent)loLoader.load();
-        aoPrimaryStage.setTitle("CameraFeed");
-        aoPrimaryStage.setScene(new Scene(loRoot, 700, 400));
-        aoPrimaryStage.show();
+    }
 
-        //System.exit(0);
+    @Override
+    public void stop() {
+        System.exit(0);
     }
 }
